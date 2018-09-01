27155e781612cdbec9d5285ad4069afd17843f82
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-09-01 11:51:31 +0430

    Add some imports 1

--- TTS-Voipi.java
+++ TTS-Voipi.java
@@ -1,1 +1,25 @@
-elkd
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import java.awt.Color;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Window;
+import javax.swing.SwingConstants;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;

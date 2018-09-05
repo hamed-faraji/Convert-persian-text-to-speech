2c18b173540143d951d546fe1daf767154c389a0
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-09-05 22:38:31 +0430

    Button to add voice to database

--- TTS-Voipi.java
+++ TTS-Voipi.java
@@ -484,6 +484,23 @@ public class TTS-Voipi extends JFrame {
 			btnNewButton.setBorder(new MatteBorder(3, 3, 3, 3, (Color) new Color(0, 0, 0)));
 			btnNewButton.setBounds(409, 599, 41, 36);
 			contentPane1.add(btnNewButton);
+			Image scaleImage2 = icon.getImage().getScaledInstance(70, 70, Image.SCALE_DEFAULT);
+			
+			////////////////////////////////// Button to add voice to database
+			JButton btnNewButton_1 = new JButton("");
+			btnNewButton_1.setFocusable(false);
+			btnNewButton_1.setFocusTraversalKeysEnabled(false);
+			btnNewButton_1.setFocusPainted(false);
+			btnNewButton_1.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+				}
+			});
+			btnNewButton_1.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+			btnNewButton_1.setBackground(Color.WHITE);
+			btnNewButton_1.setBounds(268, 686, 136, 31);
+			btnNewButton_1.setIcon(new ImageIcon(scaleImage2));
+			contentPane1.add(btnNewButton_1);
 		}
 		
 	/////////////////////////////////////// page to get your text's related voice from database ///////////	
